61b523604d60a7e2f4088984e594d36ea27db061
Douglas De León <douglasdeleon@Douglass-MacBook-Pro.local>
2021-08-31 18:17:58 -0600

    logging

--- routing.py
+++ routing.py
@@ -2,6 +2,7 @@ import sys
 import json
 import asyncio
 import argparse
+import logging
 from getpass import getpass
 
 from aioconsole import ainput
@@ -286,6 +287,14 @@ if __name__=='__main__':
     # JID and password options.
     parser.add_argument("--alg", dest='alg', help="Routing algorithm to use. Can be 'flooding', 'dv' or 'ls'.")
 
+    # Output verbosity options.
+    parser.add_argument("-q", "--quiet", help="set logging to ERROR",
+                        action="store_const", dest="loglevel",
+                        const=logging.ERROR, default=logging.INFO)
+    parser.add_argument("-d", "--debug", help="set logging to DEBUG",
+                        action="store_const", dest="loglevel",
+                        const=logging.DEBUG, default=logging.INFO)
+
     parser.add_argument("-j", "--jid", dest="jid", help="JID to use")
     parser.add_argument("-p", "--password", dest="password", help="password to use")
     
@@ -293,6 +302,10 @@ if __name__=='__main__':
     parser.add_argument("-t", "--topo", dest="topo", help="Topology filename.")
 
     args = parser.parse_args()
+
+    # Setup logging.
+    logging.basicConfig(level=args.loglevel,
+                        format='%(levelname)-8s %(message)s')
 
     if args.jid is None:
         args.jid = input("Username: ")
